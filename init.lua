387c2b00a7408da25fed6d8aaa9471f54bf1d9b6
Khalid Alansary <khaled.ansary2003@outlook.com>
2024-09-20 12:47:46 +0300

    :wq automatically creates needed directories

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 require("config")
+
+-- :wq automatically creates needed directories
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		local dir = vim.fn.expand("<afile>:p:h")
+		if vim.fn.isdirectory(dir) == 0 then
+			vim.fn.mkdir(dir, "p")
+		end
+	end,
+})
