95cba9bd17e7b4a3803ef8ead0feb22f9aee43af
Khalid Alansary <khaled.ansary2003@outlook.com>
2024-11-10 17:20:19 +0200

    switch colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	opts = {
 		transparent_background = false,
 	},
 	config = function(_, opts)
-		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin")
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight")
 	end,
 }
